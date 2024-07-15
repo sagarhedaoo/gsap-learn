aa5a2d2958b627e99e8774d7f0406fba880d3e4e
Sagar Hedaoo <sagarhedaoo@gmail.com>
2024-07-15 17:35:27 -0400

    gsapRef

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="h-screen w-screen">
       <Hero />
     </div>
   );
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+"use client";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import React, { useRef } from "react";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  const gsapRef = useRef();
+
+  useGSAP(() => {
+    gsap.to(gsapRef.current, {
+      x: 500,
+      y: 500,
+      duration: 2,
+      delay: 1,
+      rotate: 720,
+    });
+  });
+
+  return (
+    <main className="h-screen w-full bg-black p-3 ">
+      <div
+        ref={gsapRef}
+        className="box h-[300px] w-[300px] bg-red-400 rounded-lg"
+      ></div>
+    </main>
+  );
 };
 
 export default Hero;
